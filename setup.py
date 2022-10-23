782f2d0b236559d1acb9e661f211b5190d540932
DarkNeuroscience <85752632+DarkNeuroscience@users.noreply.github.com>
2022-10-23 16:19:50 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ author_email = 'neuroscience@sigmund.science',
 description = 'Business, Finance and Universal Tax Tools. UK Specific Tax Tools also Included',
 url = 'https://github.com/NeuroscienceAfterDark/MoneyAfterDark',
 download_url = 'https://github.com/NeuroscienceAfterDark/MoneyAfterDark/archive/refs/tags/1.0.6.tar.gz',
-install_requires=['os'
+install_requires=['os',
                   'pandas',
                   'numpy',],
 classifiers = ['Programming Language :: Python :: 3',
