4f36e9cca0a97d41af773ef7f0d9c87ab53788b1
DarkNeuroscience <85752632+DarkNeuroscience@users.noreply.github.com>
2022-11-10 19:06:05 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+setup(
+name = 'MoneyAfterDark',
+packages = find_packages(exclude=['test']),
+version = '1.0.9',
+author = 'NeuroscienceAfterDark',
+author_email = 'neuroscience@sigmund.science',
+description = 'Business, Finance and Universal Tax Tools. UK Specific Tax Tools also Included',
+url = 'https://github.com/NeuroscienceAfterDark/MoneyAfterDark',
+download_url = 'https://github.com/NeuroscienceAfterDark/MoneyAfterDark/archive/refs/tags/1.0.9.tar.gz',
+install_requires=['os',
+                  'pandas',
+                  'numpy',
+                  'plotly',
+                  'datetime'],
+classifiers = ['Programming Language :: Python :: 3',
+               'License :: OSI Approved :: MIT License',
+               'Operating System :: OS Independent',
+               ],
+)
---
+++ src/MoneyAfterDark/__init__.py
@@ -0,0 +1,1 @@
+from MoneyAfterDark.main import BusinessTools, TaxTools
